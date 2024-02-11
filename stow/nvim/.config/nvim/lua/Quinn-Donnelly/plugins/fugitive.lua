6fc60ebfd776b2f551d2764a8eaf57346b1d22ec
Quinn Donnelly <quinndonnelly22@gmail.com>
2024-02-10 21:28:18 -0500

    feat: adding fugitive keybind to trigger git

--- stow/nvim/.config/nvim/lua/Quinn-Donnelly/plugins/fugitive.lua
+++ stow/nvim/.config/nvim/lua/Quinn-Donnelly/plugins/fugitive.lua
@@ -1,5 +1,8 @@
 return {
     "tpope/vim-fugitive",
+    keys = {
+        { "<leader>gs", "<cmd>G<cr>", desc = "Open Fugitive" },
+    },
     dependencies = {
         "tpope/vim-rhubarb",
     },
