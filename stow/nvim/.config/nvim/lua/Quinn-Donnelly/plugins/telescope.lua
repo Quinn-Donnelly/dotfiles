c7772dad79a24ccb4c83c945a0e44b666644c8b2
Quinn Donnelly <quinndonnelly22@gmail.com>
2024-05-28 02:11:45 -0400

    feat: adding leader ph key to search help docs

--- stow/nvim/.config/nvim/lua/Quinn-Donnelly/plugins/telescope.lua
+++ stow/nvim/.config/nvim/lua/Quinn-Donnelly/plugins/telescope.lua
@@ -1,25 +1,26 @@
 return {
-	"nvim-telescope/telescope.nvim",
+    "nvim-telescope/telescope.nvim",
 
-	dependencies = {
-		"nvim-lua/plenary.nvim"
-	},
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
 
-	keys = {
-		{ "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "find files" },
-		{ "<C-p>", "<cmd>Telescope git_files<cr>", desc = "find files checked into git" },
-		{ "<leader>ps", "<cmd>Telescope live_grep<cr>", desc = "grep for pattern" },
-	},
+    keys = {
+        { "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "find files" },
+        { "<C-p>",      "<cmd>Telescope git_files<cr>",  desc = "find files checked into git" },
+        { "<leader>ps", "<cmd>Telescope live_grep<cr>",  desc = "grep for pattern" },
+        { "<leader>ph", "<cmd>Telescope help_tags<cr>",  desc = "search help" },
+    },
 
-	config = function()
-        require("telescope").setup { 
-        pickers = {
-            live_grep = {
-                additional_args = function(opts)
-                    return {"--hidden"}
-                end
+    config = function()
+        require("telescope").setup {
+            pickers = {
+                live_grep = {
+                    additional_args = function(opts)
+                        return { "--hidden" }
+                    end
+                },
             },
-        },
         }
-	end
+    end
 }
--- stow/nvim/.config/nvim/lua/Quinn-Donnelly/remap.lua
+++ stow/nvim/.config/nvim/lua/Quinn-Donnelly/remap.lua
@@ -15,7 +15,7 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader><leader>", function()
+vim.keymap.set("n", "<leader>r", function()
     vim.cmd("so")
 end)
 
