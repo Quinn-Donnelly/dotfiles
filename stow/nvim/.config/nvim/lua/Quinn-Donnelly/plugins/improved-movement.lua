e2beeda40bd39b222de8ccb66f54b84d7d496062
Quinn Donnelly <quinndonnelly22@gmail.com>
2024-06-09 17:25:42 -0400

    feat: adding improved movement commands
    - note that treesitter objects aren't set up yet

---
+++ stow/nvim/.config/nvim/lua/Quinn-Donnelly/plugins/improved-movement.lua
@@ -0,0 +1,7 @@
+return {
+    "echasnovski/mini.ai",
+    config = function()
+        -- TODO: I can add treesitter objects here to further improve the defaults
+        require('mini.ai').setup()
+    end
+}
